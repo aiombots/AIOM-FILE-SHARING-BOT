a3dbc91f84b447e75955d543ff3cd5892b331a7c
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-10 17:05:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,29 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/ajvadntr'>@ajvadntr</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/ajvadntr/AIOM-FILE-SHARING-BOT'>Click here</a>\n○ Channel : @AIOM_BOTS\n○ Group : @AIOM_BOTS_GROUP</b>",
+            text = f"""<b>╭────[ Vɪɴᴄᴇɴᴢ๏ ]───⍟
+│
+├ Bᴏᴛ Nᴀᴍᴇ : <a href="https://t.me/VINCENZO_CASSANO_ROBOT">Vɪɴᴄᴇɴᴢ๏</a>
+│
+├ Cʜᴀɴɴᴇʟ : <a href="https://t.me/AIOM_BOTS">ƛƖƠM ƁƠƬƧ"</a>
+│
+├ Vᴇʀsɪᴏɴ : 1.0.2.2  Bᴇᴛᴀ
+│
+├ Sᴇʀᴠᴇʀ : <a href="https://www.heroku.com">Hᴇʀᴏᴋᴜ</a>
+│
+├ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href="https://t.me/ajvadntr">Cʟɪᴄᴋ Hᴇʀᴇ</a>
+│
+├ Pᴏᴡᴇʀᴇᴅ Bʏ : <a href="https://t.me/AIOM_BOTS">ƛƖƠM ƁƠƬƧ"</a>
+│
+╰──────[ Tʜᴀɴᴋ Yᴏᴜ ]───⍟</b>""",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("⛔️ Close", callback_data = "close")
+                        InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/AIOM_BOTS"),
+                        InlineKeyboardButton("Gʀᴏᴜᴘ", url="https://t.me/AllinOneMoviesTalks")
+                    ],[
+                        InlineKeyboardButton("Cʟᴏsᴇ", callback_data = "close")
                     ]
                 ]
             )
